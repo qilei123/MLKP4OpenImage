6f650b5d6531f427b832ae619cd19f41ac2922f0
qilei123 <qileimail123@gmail.com>
2018-07-10 00:55:40 -0400

    dsf

--- lib/setup.py
+++ lib/setup.py
@@ -48,7 +48,8 @@ def locate_cuda():
         home = os.path.dirname(os.path.dirname(nvcc))
 
     cudaconfig = {'home':home, 'nvcc':nvcc,
-                  'include': pjoin(home, 'targets/x86_64-linux/include'),
+                  #'include': pjoin(home, 'targets/x86_64-linux/include'),
+                  'include': pjoin(home, 'include'),
                   'lib64': pjoin(home, 'lib64')}
     for k, v in cudaconfig.iteritems():
         if not os.path.exists(v):
