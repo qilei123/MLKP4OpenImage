2439ca21f19439b97bb280be5d602cc8f98528dc
qilei123 <qileimail123@gmail.com>
2018-07-14 12:04:37 -0400

    ljfsda

--- train_multigpus.sh
+++ train_multigpus.sh
@@ -1,5 +1,5 @@
 python ./tools/train_net_multi_gpu.py --gpu 2,3\
         --solver models/VGG16/solver.prototxt\
-        --weights data/ImageNet_models/VGG16_faster_rcnn_final.caffemodel\
+        --weights output/faster_rcnn_end2end/voc_2007_trainvalls/vgg16_mlkp_iter_40000.caffemodel\
         --imdb voc_2007_trainval\
         --cfg experiments/cfgs/faster_rcnn_end2end.yml
