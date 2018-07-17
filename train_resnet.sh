243002225c35bf524c096547eb8fb4fc2837efa4
qilei123 <qileimail123@gmail.com>
2018-07-17 14:16:46 -0400

    fdsafd

---
+++ train_resnet.sh
@@ -0,0 +1,5 @@
+python ./tools/train_resnet.py --gpu 0\
+    --solver models/ResNet/solver.prototxt\
+    --weights models/ResNet/ResNet-101-model.caffemodel\
+    --imdb voc_2007_trainval\
+    --cfg experiments/cfgs/faster_rcnn_end2end.yml
