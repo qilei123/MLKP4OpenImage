bd09ce4c95967f236fe3d7af84a430dd377e2940
qilei123 <qileimail123@gmail.com>
2018-07-17 03:35:18 -0400

    fdsafd

--- train_multigpus_resnet.sh
+++ train_multigpus_resnet.sh
@@ -1,5 +1,5 @@
 python ./tools/train_net_multi_gpu_resnet.py --gpu 0,1\
         --solver models/ResNet/solver.prototxt\
-        --weights /home/qileimail123/Development/openimagev4/CVDF/ResNet-101-model.caffemodel\
+        --weights /home/qileimail123/openimagev4/MLKP4OpenImage/models/ResNet/ResNet-101-model.caffemodel\
         --imdb voc_2007_trainval\
         --cfg experiments/cfgs/rfcn_end2end_ohem.yml
