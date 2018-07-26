40aba41d49dca284235c48f421ec3e5d26882719
qilei123 <qileimail123@gmail.com>
2018-07-25 23:02:25 -0400

    qileifadsf

--- train_resnet.sh
+++ train_resnet.sh
@@ -2,4 +2,4 @@ python ./tools/train_resnet.py --gpu 1\
     --solver models/ResNet/solver.prototxt\
     --weights models/ResNet/ResNet-101-model.caffemodel\
     --imdb voc_2007_trainval\
-    --cfg experiments/cfgs/faster_rcnn_end2end_resnet.yml
+    --cfg experiments/cfgs/rfcn_end2end.yml
