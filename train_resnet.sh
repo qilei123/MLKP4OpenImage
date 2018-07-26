441a6d54c7c65df2e9e5094719e1bbeb2b23f66d
qilei123 <qileimail123@gmail.com>
2018-07-25 22:48:28 -0400

    dfsds

--- train_resnet.sh
+++ train_resnet.sh
@@ -1,5 +1,5 @@
-python ./tools/train_resnet.py --gpu 0\
+python ./tools/train_resnet.py --gpu 1\
     --solver models/ResNet/solver.prototxt\
     --weights models/ResNet/ResNet-101-model.caffemodel\
     --imdb voc_2007_trainval\
-    --cfg experiments/cfgs/rfcn_end2end.yml
+    --cfg experiments/cfgs/faster_rcnn_end2end_resnet.yml
